2ac5b71e0a6fbf3db77223d8e508b2988dfcbe42
Mateus Santos <ferrmathh2@gmail.com>
2024-04-21 20:40:19 -0300

    feat: add logic to make a sheet with infos of the html using spout

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -2,6 +2,9 @@
 
 namespace Chuva\Php\WebScrapping;
 
+use Box\Spout\Writer\Common\Creator\WriterEntityFactory;
+use Box\Spout\Common\Entity\Row;
+
 /**
  * Runner for the Webscrapping exercice.
  */
@@ -14,10 +17,35 @@ class Main {
     $dom = new \DOMDocument('1.0', 'utf-8');
     $dom->loadHTMLFile(__DIR__ . '/../../assets/origin.html');
 
-    $data = (new Scrapper())->scrap($dom);
+    $papers = (new Scrapper())->scrap($dom);
 
-    // Write your logic to save the output file bellow.
-    print_r($data);
+    $writer = WriterEntityFactory::createXLSXWriter();
+    $writer->openToFile(__DIR__ . '/../../assets/model-result.xlsx');
+
+    $headerRow = WriterEntityFactory::createRowFromArray(
+      ['ID', 'Title', 'Type',
+        'Author 1', 'Author 1 Institution',
+        'Author 2', 'Author 2 Institution',
+        'Author 3', 'Author 3 Institution',
+        'Author 4', 'Author 4 Institution',
+        'Author 5', 'Author 5 Institution',
+        'Author 6', 'Author 6 Institution',
+        'Author 7', 'Author 7 Institution',
+        'Author 8', 'Author 8 Institution',
+        'Author 9', 'Author 9 Institution']
+    );
+
+    $writer->addRow($headerRow);
+
+    foreach($papers as $paper) {
+      $dataRow = WriterEntityFactory::createRowFromArray([$paper->id, $paper->title, $paper->type]);
+      foreach($paper->authors as $author) {
+        $dataRow->addCell(WriterEntityFactory::createCell($author->name));
+        $dataRow->addCell(WriterEntityFactory::createCell($author->institution));
+      }
+      $writer->addRow($dataRow);
+    }
+    $writer->close();
   }
 
 }
