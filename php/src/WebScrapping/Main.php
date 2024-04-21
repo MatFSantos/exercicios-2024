b2886ff2fef66ff1344cc7f8e2f64a3e4846f177
Mateus Santos <ferrmathh2@gmail.com>
2024-04-21 20:54:33 -0300

    ajust in comment position and closed parentesis of array

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -21,18 +21,19 @@ class Main {
     $writer = WriterEntityFactory::createXLSXWriter();
     $writer->openToFile(__DIR__ . '/../../assets/model-result.xlsx');
 
-    $headerRow = WriterEntityFactory::createRowFromArray(
-      ['ID', 'Title', 'Type',
-        'Author 1', 'Author 1 Institution',
-        'Author 2', 'Author 2 Institution',
-        'Author 3', 'Author 3 Institution',
-        'Author 4', 'Author 4 Institution',
-        'Author 5', 'Author 5 Institution',
-        'Author 6', 'Author 6 Institution',
-        'Author 7', 'Author 7 Institution',
-        'Author 8', 'Author 8 Institution',
-        'Author 9', 'Author 9 Institution']
-    ); // cabeçalho feito com base na assets/model.xlsx
+    // cabeçalho feito com base na assets/model.xlsx.
+    $headerRow = WriterEntityFactory::createRowFromArray([
+      'ID', 'Title', 'Type',
+      'Author 1', 'Author 1 Institution',
+      'Author 2', 'Author 2 Institution',
+      'Author 3', 'Author 3 Institution',
+      'Author 4', 'Author 4 Institution',
+      'Author 5', 'Author 5 Institution',
+      'Author 6', 'Author 6 Institution',
+      'Author 7', 'Author 7 Institution',
+      'Author 8', 'Author 8 Institution',
+      'Author 9', 'Author 9 Institution'
+    ]);
 
     $writer->addRow($headerRow);
 
